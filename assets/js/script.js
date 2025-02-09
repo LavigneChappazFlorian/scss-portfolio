835b8fe74c7399bf68861182a68016cb9e54558e
Florian Lavigne Chappaz <florian.lavigne.chappaz@gmail.com>
2025-02-09 18:22:35 +0100

    add dark mode + animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
-let iconBurgerOn = document.getElementById('burger');
-let menu = document.getElementById('menu')
+const iconBurgerOn = document.getElementById('burger');
+const menu = document.getElementById('menu')
 
 iconBurgerOn.addEventListener('click', () => {
     menu.classList.toggle('active');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("toggle-dark-mode");
+    const body = document.body;
+    const navTitle = document.getElementById("nav-title");
+    const navGithub = document.getElementById("nav-github");
+    const navLinkedin = document.getElementById("nav-linkedin");
+    const navMail = document.getElementById("nav-mail");
+
+    // Vérifier le mode enregistré
+    if (localStorage.getItem("dark-mode") === "enabled") {
+        body.classList.add("dark");
+    }
+
+    toggleButton.addEventListener("click", () => {
+        body.classList.toggle("dark");
+        navTitle.classList.toggle('dark');
+        navGithub.classList.toggle('dark');
+        navLinkedin.classList.toggle('dark');
+        navMail.classList.toggle('dark');
+        toggleButton.classList.toggle('dark');
+
+        if (body.classList.contains("dark")) {
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            localStorage.removeItem("dark-mode");
+        }
+    });
 });
